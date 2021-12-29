895b48b2cbd69f10f4b2531781507799bd7933cc
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-29 22:31:32 +0300

    add description

--- fundamentals/Isograms.js
+++ fundamentals/Isograms.js
@@ -1,14 +1,22 @@
+/* eslint-disable no-unused-vars */
+
 /*
-An isogram is a word that has no repeating letters, 
-consecutive or non-consecutive. Implement a function 
-that determines whether a string that contains only 
-letters is an isogram. Assume the empty string is an 
+An isogram is a word that has no repeating letters,
+consecutive or non-consecutive. Implement a function
+that determines whether a string that contains only
+letters is an isogram. Assume the empty string is an
 isogram. Ignore letter case.
 */
 
-function isIsogram(str){
+function isIsogram(str) {
   if (str.length === 0) {
     return true;
   }
   return str.length === new Set(str.toLowerCase()).size;
 }
+
+/*
+1) object Set() returns a string without duplicate characters;
+2) method .toLowerCase() bring characters to the same case (lower);
+3) compare string length and new object's length with unique chars;
+*/
